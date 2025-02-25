d8a1611a7286c16d0726d74c76df6a59456ec26a
Fernando Santana <bh.carvalho@gmail.com>
2025-02-25 19:25:00 -0300

    fix routes api configuration

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,22 @@
 use App\Http\Controllers\LivroController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('livros', LivroController::class);
+// Route::apiResource('livros', LivroController::class);
 
 Route::get('livros', [LivroController::class, 'index']);
 Route::get('livros/{livro}', [LivroController::class, 'show']);
 Route::post('livros', [LivroController::class, 'store']);
 Route::put('livros/{livro}', [LivroController::class, 'update']);
 Route::delete('livros/{livro}', [LivroController::class, 'destroy']);
+
+Route::get('livros2', function(){
+    echo 'test';
+});
+
+// Route::middleware('api')->group(function () {
+//     Route::get('livros', [LivroController::class, 'index']);
+//     Route::get('livros/{livro}', [LivroController::class, 'show']);
+//     Route::post('livros', [LivroController::class, 'store']);
+//     Route::put('livros/{livro}', [LivroController::class, 'update']);
+//     Route::delete('livros/{livro}', [LivroController::class, 'destroy']);
+// });
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LivroController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('livros', [LivroController::class, 'index']);
+// Route::get('livros/{livro}', [LivroController::class, 'show']);
+// Route::post('livros', [LivroController::class, 'store']);
+// Route::put('livros/{livro}', [LivroController::class, 'update']);
+// Route::delete('livros/{livro}', [LivroController::class, 'destroy']);
